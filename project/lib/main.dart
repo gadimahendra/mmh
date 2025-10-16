c9b24f1d413c0e0dc5826e0081ff12cfcb70e75b
gadimahendra <gadi.mahendra@outlook.com>
2025-10-16 13:40:09 +0200

    Fix route naming conflicts, resolve compilation errors, and add .gitignore

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // --- Screens ---
 import 'screens/Manufacture.dart';
-import 'screens/Manufacture1.dart';
+// import 'screens/Manufacture1.dart';
 import 'screens/BuyerManagement.dart';
 import 'screens/SellerManagement.dart';
 import 'screens/SellerLogin.dart';
@@ -12,30 +12,30 @@ import 'screens/SellerListProducts.dart';
 import 'screens/SellerUpdateProducts.dart';
 import 'screens/SellerAddProducts.dart';
 import 'screens/Div.dart';
-import 'screens/Div1.dart';
-import 'screens/Div3.dart';
+// import 'screens/Div1.dart';
+// import 'screens/Div3.dart';
 import 'screens/SellerOrder.dart';
-import 'screens/update_order.dart';
+// import 'screens/update_order.dart';
 import 'screens/Details.dart';
 import 'screens/Frame1000003000.dart';
 import 'screens/SellerProfile.dart';
 import 'screens/Return.dart';
-import 'screens/Return1.dart';
+// import 'screens/Return1.dart';
 import 'screens/SignIn.dart';
 import 'screens/SignUp.dart';
 import 'screens/Home.dart';
 import 'screens/Profile.dart';
-import 'screens/NotificationPage.dart';
+// import 'screens/NotificationPage.dart'; File not exist
 import 'screens/Frame.dart';
-import 'screens/Frame1.dart';
-import 'screens/Frame2.dart';
-import 'screens/Frame3.dart';
-import 'screens/Frame4.dart';
-import 'screens/Frame5.dart';
-import 'screens/Frame6.dart';
-import 'screens/Frame7.dart';
-import 'screens/Frame8.dart';
-import 'screens/Frame9.dart';
+// import 'screens/Frame1.dart';
+// import 'screens/Frame2.dart';
+// import 'screens/Frame3.dart';
+// import 'screens/Frame4.dart';
+// import 'screens/Frame5.dart';
+// import 'screens/Frame6.dart';
+// import 'screens/Frame7.dart';
+// import 'screens/Frame8.dart';
+// import 'screens/Frame9.dart';
 import 'screens/ProductListPage.dart';
 import 'screens/SearchPage.dart';
 import 'screens/ProductsDetailsPage.dart';
@@ -50,10 +50,9 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // --- Centralized route map ---
   static final Map<String, Widget Function(BuildContext)> appRoutes = {
     '/Manufacture': (context) => const Manufacture(),
-    '/Manufacture1': (context) => const Manufacture1(),
+    // '/Manufacture1': (context) => const Manufacture1(),
     '/BuyerManagement': (context) => const BuyerManagement(),
     '/SellerManagement': (context) => const SellerManagement(),
     '/SellerLogin': (context) => const SellerLogin(),
@@ -66,23 +65,23 @@ class MyApp extends StatelessWidget {
     '/SellerAddProducts': (context) => const SellerAddProducts(),
     '/SellerOrder': (context) => const SellerOrder(),
     '/SellerProfile': (context) => const SellerProfile(),
-    '/update_order': (context) => const update_order(),
+    // '/update_order': (context) => const update_order(),
     '/Return': (context) => const Return(),
-    '/Return1': (context) => const Return1(),
+    // '/Return1': (context) => const Return1(),
     '/OrderHistory': (context) => const OrderHistory(),
     '/Div': (context) => const Div(),
-    '/Div1': (context) => const Div1(),
-    '/Div3': (context) => const Div3(),
+    // '/Div1': (context) => const Div1(),
+    // '/Div3': (context) => const Div3(),
     '/Frame': (context) => const Frame(),
-    '/Frame1': (context) => const Frame1(),
-    '/Frame2': (context) => const Frame2(),
-    '/Frame3': (context) => const Frame3(),
-    '/Frame4': (context) => const Frame4(),
-    '/Frame5': (context) => const Frame5(),
-    '/Frame6': (context) => const Frame6(),
-    '/Frame7': (context) => const Frame7(),
-    '/Frame8': (context) => const Frame8(),
-    '/Frame9': (context) => const Frame9(),
+    // '/Frame1': (context) => const Frame1(),
+    // '/Frame2': (context) => const Frame2(),
+    // '/Frame3': (context) => const Frame3(),
+    // '/Frame4': (context) => const Frame4(),
+    // '/Frame5': (context) => const Frame5(),
+    // '/Frame6': (context) => const Frame6(),
+    // '/Frame7': (context) => const Frame7(),
+    // '/Frame8': (context) => const Frame8(),
+    // '/Frame9': (context) => const Frame9(),
     '/Frame1000003000': (context) => const Frame1000003000(),
     '/Home': (context) => const Home(),
     '/ProductListPage': (context) => const ProductListPage(),
@@ -92,7 +91,7 @@ class MyApp extends StatelessWidget {
     '/Filter': (context) => const Filter(),
     '/SortBy': (context) => const SortBy(),
     '/Profile': (context) => const Profile(),
-    '/Notification': (context) => const NotificationPage(),
+    // '/Notification': (context) => const NotificationPage(),
     '/MainDashboard': (context) => const MainDashboard(),
     '/Details': (context) => const Details(),
   };
